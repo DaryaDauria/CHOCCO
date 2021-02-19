34e083f1cb3c69f44e3af3d1489332794771063a
DaryaDauria <72822755+DaryaDauria@users.noreply.github.com>
2021-02-19 12:45:11 +0300

    Add files via upload

---
+++ gulpfile.js
@@ -0,0 +1,60 @@
+const { src, dest, task, series, watch } = require("gulp");
+const rm = require("gulp-rm");
+const sass = require("gulp-sass");
+
+const concat = require("gulp-concat");
+const browserSync = require("browser-sync").create();
+const reload = browserSync.reload;
+const sassGlob = require("gulp-sass-glob");
+const autoprefixer = require("gulp-autoprefixer");
+
+sass.compiler = require("node-sass");
+
+task("clean", () => {
+  return src("dist/**/*", { read: false }).pipe(rm());
+});
+
+task("copy:html", () => {
+  return src("src/*.html")
+    .pipe(dest("dist"))
+    .pipe(reload({ stream: true }));
+});
+
+const styles = [
+  "node_modules/normalize.css/normalize.css",
+  "src/styles/css-components/main.scss",
+];
+
+task("styles", () => {
+  return src(styles)
+    .pipe(concat("main.scss"))
+    .pipe(sassGlob())
+    .pipe(sass().on("error", sass.logError))
+    .pipe(
+      autoprefixer({
+        browsers: ["last 2 versions"],
+        cascade: false,
+      })
+    )
+    .pipe(dest("dist"));
+});
+
+task("scripts", () => {
+  return src("src/scripts/*.js")
+    .pipe(sourcemaps.init())
+    .pipe(concat("main.js", { newLine: ";" }))
+    .pipe(sourcemaps.write())
+    .pipe(dest("dist"));
+});
+
+task("server", () => {
+  browserSync.init({
+    server: {
+      baseDir: "./dist",
+    },
+    open: false,
+  });
+});
+watch("src/**/*.scss", series("styles"));
+watch("./src/*.html", series("copy:html"));
+task("default", series("clean", "copy:html", "styles", "server"));
